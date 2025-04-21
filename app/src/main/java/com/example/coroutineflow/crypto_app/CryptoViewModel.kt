df9aa51bb59247f1558c45a3c4c9cf3661da1d1f
k8abadzheva <make.me.loco@gmail.com>
2025-04-21 23:24:42 +0300

    feat: Load data in CryptoViewModel and call loadData() and stopLoading() in CryptoActivity.

--- app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,27 +1,38 @@
 package com.example.coroutineflow.crypto_app
 
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    val state: LiveData<State> = repository.getCurrencyList()
-        .filter { it.isNotEmpty() }
-        .map { State.Content(currencyList = it) as State }
-        .onStart { emit(State.Loading) }
-        .asLiveData()
+    private val _state = MutableLiveData<State>(State.Initial)
+    val state: LiveData<State> = _state
+
+    private var job: Job? = null
+
+    fun loadData() {
+        job = repository.getCurrencyList()
+            .onStart { _state.value = State.Loading }
+            .filter { it.isNotEmpty() }
+            .onEach {
+                Log.d("CryptoViewModel", "onEach: $it")
+                _state.value = State.Content(currencyList = it)
+            }
+            .launchIn(viewModelScope)
+    }
+
+    fun stopLoading() {
+        job?.cancel()
+    }
 }
