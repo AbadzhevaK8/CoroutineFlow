6568c25be890cfe45664a972c4a1881c195e4c27
k8abadzheva <make.me.loco@gmail.com>
2025-04-22 23:55:37 +0300

    Refactor CryptoViewModel to manage data loading state and lifecycle using Kotlin Flow onStart and onCompletion. Specifically the following changes were made: - Use `onStart` to indicate the start of data loading. - Add `onCompletion` to log the completion of the flow. - Added isResumed, cancel the job and reset the job to null after 5 seconds delay in `stopLoading()` to avoid data load if fragment is paused or the activity is gone. - Added job check in `loadData()` to avoid multiple job run.

--- app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -7,10 +7,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class CryptoViewModel : ViewModel() {
 
@@ -20,19 +23,36 @@ class CryptoViewModel : ViewModel() {
     val state: LiveData<State> = _state
 
     private var job: Job? = null
+    private var isResumed = false
 
     fun loadData() {
+        isResumed = true
+        if (job != null) {
+            return
+        }
         job = repository.getCurrencyList()
-            .onStart { _state.value = State.Loading }
+            .onStart {
+                Log.d("CryptoViewModel", "Started")
+                _state.value = State.Loading
+            }
             .filter { it.isNotEmpty() }
             .onEach {
                 Log.d("CryptoViewModel", "onEach: $it")
                 _state.value = State.Content(currencyList = it)
             }
+            .onCompletion { Log.d("CryptoViewModel", "Complete") }
             .launchIn(viewModelScope)
     }
 
     fun stopLoading() {
-        job?.cancel()
+        viewModelScope.launch {
+            delay(5000)
+            if (!isResumed) {
+                job?.cancel()
+                job = null
+            } else {
+                isResumed = false
+            }
+        }
     }
 }
