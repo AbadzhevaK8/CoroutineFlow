dc5225f3b80cb27334a5861d8b8156527b0ebb60
k8abadzheva <make.me.loco@gmail.com>
2025-04-29 22:56:09 +0300

    Add Lesson8.kt to demonstrate a basic Kotlin Flow emitting and collecting integer values with a delay.

---
+++ app/src/main/java/com/example/coroutineflow/lessons/lesson8/Lesson8.kt
@@ -0,0 +1,20 @@
+package com.example.coroutineflow.lessons.lesson8
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+suspend fun main() {
+    val flow = getFlow()
+    flow.collect {
+        println("Collected: $it")
+    }
+}
+
+fun getFlow(): Flow<Int> = flow {
+    repeat(100) {
+        println("Emitted: $it")
+        emit(it)
+        delay(1000)
+    }
+}
