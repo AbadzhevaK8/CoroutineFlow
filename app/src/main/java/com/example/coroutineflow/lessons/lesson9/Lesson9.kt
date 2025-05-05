5abb322ebbc15ff4aed064fd750262bb1f7522e3
k8abadzheva <make.me.loco@gmail.com>
2025-05-05 22:27:23 +0300

    Refactor: Implement `MutableSharedFlow` collection in Lesson9
    Introduce multiple collectors for `MutableSharedFlow` in `Lesson9.kt`. Each collector receives emitted values. The first collector begins immediately, while the second collector starts after a 5-second delay. The emitter was also modified to run 5 times instead of 100.

--- app/src/main/java/com/example/coroutineflow/lessons/lesson9/Lesson9.kt
+++ app/src/main/java/com/example/coroutineflow/lessons/lesson9/Lesson9.kt
@@ -15,18 +15,18 @@ suspend fun main() {
     val flow = MutableSharedFlow<Int>()
 
     coroutineScope.launch {
-        repeat(100) {
+        repeat(5) {
             println("Emitted: $it")
             flow.emit(it)
             delay(1000)
         }
     }
 
-//    val job1 = coroutineScope.launch {
-//        flow.collect {
-//            println("Got from 1st collector:  $it")
-//        }
-//    }
+    val job1 = coroutineScope.launch {
+        flow.collect {
+            println("Got from 1st collector:  $it")
+        }
+    }
     delay(5000)
     val job2 = coroutineScope.launch {
         flow.collect {
@@ -34,12 +34,12 @@ suspend fun main() {
         }
     }
 
-//    job1.join()
+    job1.join()
     job2.join()
 }
 
 fun getFlow(): Flow<Int> = flow {
-    repeat(100) {
+    repeat(5) {
         println("Emitted: $it")
         emit(it)
         delay(1000)
