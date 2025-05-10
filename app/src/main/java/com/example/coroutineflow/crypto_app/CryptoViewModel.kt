cd8dbf72f665addc839141d6d27cd4c29cdffbdc
k8abadzheva <make.me.loco@gmail.com>
2025-05-10 23:48:33 +0300

    Cast repository.currencyListFlow to MutableSharedFlow.
    The `currencyListFlow` property in `CryptoViewModel` is now cast to `MutableSharedFlow<List<Currency>>`.

--- app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -4,6 +4,7 @@ package com.example.coroutineflow.crypto_app
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
@@ -19,7 +20,7 @@ class CryptoViewModel : ViewModel() {
         }
     }
 
-    val state: Flow<State> = repository.currencyListFlow
+    val state: Flow<State> = (repository.currencyListFlow as MutableSharedFlow<List<Currency>>)
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
         .onStart { emit(State.Loading) }
