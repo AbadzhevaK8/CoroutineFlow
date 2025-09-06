654d05a9326e4c312301aed2727b3b8a77db88ca
k8abadzheva <make.me.loco@gmail.com>
2025-09-06 22:43:52 +0300

    Update `StateFlow` directly instead of using `emit()`.
    The `TeamScoreViewModel` now updates the `_state` `MutableStateFlow` by directly setting its `value` property. This simplifies the state update mechanism by removing the need for `viewModelScope.launch` and `_state.emit()`.

--- app/src/main/java/com/example/coroutineflow/team_score/TeamScoreViewModel.kt
+++ app/src/main/java/com/example/coroutineflow/team_score/TeamScoreViewModel.kt
@@ -1,13 +1,8 @@
 package com.example.coroutineflow.team_score
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 
 class TeamScoreViewModel : ViewModel() {
 
@@ -15,35 +10,29 @@ class TeamScoreViewModel : ViewModel() {
     val state = _state.asStateFlow()
 
     fun increaseScore(team: Team) {
-        viewModelScope.launch {
-            val currentState = state.value
-            if (currentState is TeamScoreState.Game) {
-                if (team == Team.TEAM_1) {
-                    val oldValue = currentState.score1
-                    val newValue = oldValue + 1
-                    _state.emit(currentState.copy(score1 = newValue))
-                    if (newValue >= WINNER_SCORE) {
-                        _state.emit(
-                            TeamScoreState.Winner(
-                                winnerTeam = Team.TEAM_1,
-                                newValue,
-                                currentState.score2
-                            )
-                        )
-                    }
-                } else {
-                    val oldValue = currentState.score2
-                    val newValue = oldValue + 1
-                    _state.emit(currentState.copy(score2 = newValue))
-                    if (newValue >= WINNER_SCORE) {
-                        _state.emit(
-                            TeamScoreState.Winner(
-                                winnerTeam = Team.TEAM_2,
-                                currentState.score1,
-                                newValue
-                            )
-                        )
-                    }
+        val currentState = state.value
+        if (currentState is TeamScoreState.Game) {
+            if (team == Team.TEAM_1) {
+                val oldValue = currentState.score1
+                val newValue = oldValue + 1
+                _state.value = currentState.copy(score1 = newValue)
+                if (newValue >= WINNER_SCORE) {
+                    _state.value = TeamScoreState.Winner(
+                        winnerTeam = Team.TEAM_1,
+                        newValue,
+                        currentState.score2
+                    )
+                }
+            } else {
+                val oldValue = currentState.score2
+                val newValue = oldValue + 1
+                _state.value = currentState.copy(score2 = newValue)
+                if (newValue >= WINNER_SCORE) {
+                    _state.value = TeamScoreState.Winner(
+                        winnerTeam = Team.TEAM_2,
+                        currentState.score1,
+                        newValue
+                    )
                 }
             }
         }
